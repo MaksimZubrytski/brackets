5dbf5e2c3eb3b0b76691c712971350200bbaa5a7
MaksimZubrytski <69052702+MaksimZubrytski@users.noreply.github.com>
2021-02-24 21:37:24 +0300

    feat: solution added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsArray = bracketsConfig.map(bracketsArray => (bracketsArray.join('')))
+  let newStr = str;
+  for(let i = 0; i < bracketsArray.length; i++) {
+    if(newStr.includes(bracketsArray[i])) {
+      newStr = newStr.replace(bracketsArray[i], "")
+      i = -1
+    }
+  }
+  return !newStr;
 }
